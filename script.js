c0d581762e39ce6bfc414cbb9bb573d154f8c3b1
Michael Chan <michaelalexanderchan@gmail.com>
2023-01-17 15:04:34 -0500

    Added functions to determine the winner of the game and keep track of the amount of turns played

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Module Pattern Practice - Gameboard
+// Module pattern and IIFE practice - Gameboard
 const gameBoard = (() => {
   // Simulate a board using a 2D array
   const board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
+    [0, 'x', 'o'],
+    ['x', 'o', 'o'],
+    [0, 'x', 'o'],
   ];
 
   // Determines if a place on the board is already occupied or not
@@ -23,18 +23,89 @@ const gameBoard = (() => {
   };
 })();
 
-// Module pattern practice - Game Controller
-const gameController = (() => {
-  let turnsTaken;
-  const playerMove = (board, symbol, row, column) => {
+// Module pattern and IIFE practice - Game Controller
+const gameController = ((board) => {
+  let turnsTaken = 0;
+  console.log(`Initial turn counter: ${turnsTaken}`);
+
+  const resetTurns = () => {
+    turnsTaken = 0;
+  };
+
+  const getTurns = () => {
+    console.log(`Turns taken: ${turnsTaken}`);
+    return turnsTaken;
+  };
+
+  const playerMove = (symbol, row, column) => {
     board.addMove(row, column, symbol);
+    turnsTaken += 1;
+    getTurns();
+  };
+
+  const determineWinner = () => {
+    const currentBoard = board.board;
+    let winStatus;
+    console.log(`Array length = ${currentBoard.length}`);
+    console.log(`Row length = ${currentBoard[0].length}`);
+    console.log(`Value = ${currentBoard[0][0]}`);
+
+    // // Loop through each row
+    // for (let i = 0; i < currentBoard.length; i += 1) {
+    //   // Loop through each value in a row
+    //   // eslint-disable-next-line prefer-destructuring
+    //   winStatus = currentBoard[i][0];
+    //   for (let j = 1; j < currentBoard[i].length; j += 1) {
+    //     /* If the previous value is not equal to the current value,
+    //     there is no winner for this row and break */
+    //     if (winStatus !== currentBoard[i][j]) {
+    //       winStatus = 'no winner';
+    //       break;
+    //     } else {
+    //       winStatus = currentBoard[i][j];
+    //     }
+    //   }
+    //   if (winStatus !== 'no winner') {
+    //     break;
+    //   }
+    // }
+
+    /*  return winStatus; */
+
+    // // if (winStatus === 'no winner') {
+    // for (let i = 0; i < currentBoard.length; i += 1) {
+    //   // eslint-disable-next-line prefer-destructuring
+    //   winStatus = currentBoard[0][i];
+    //   for (let j = 1; j < currentBoard[i].length; j += 1) {
+    //     if (winStatus !== currentBoard[j][i]) {
+    //       winStatus = 'no winner';
+    //       break;
+    //     } else {
+    //       winStatus = currentBoard[j][i];
+    //     }
+    //   }
+    //   if (winStatus !== 'no winner') {
+    //     break;
+    //   }
+    // }
+    // // }
+
+    for (let i = 0; i < currentBoard.length; i += 1) {
+      winStatus = currentBoard[i][i];
+      for (let j = 1; j < currentBoard.length; j += 1) {
+
+      }
+    }
+
+    return winStatus;
   };
 
   return {
-    turnsTaken,
+    resetTurns,
     playerMove,
+    determineWinner,
   };
-})();
+})(gameBoard);
 
 // Factory Pattern Practice - Generate player objects
 const playerFactory = (name, symbol) => {
@@ -48,6 +119,9 @@ const playerFactory = (name, symbol) => {
 
 const player1 = playerFactory('Mikey', 'x');
 const player2 = playerFactory('Richie', 'o');
-gameController.playerMove(gameBoard, player1.getSymbol(), 2, 1);
-console.log(gameBoard.board);
-console.log(gameController.turnsTaken);
+gameController.playerMove(player1.getSymbol(), 2, 1);
+gameController.playerMove(player1.getSymbol(), 2, 1);
+gameController.playerMove(player1.getSymbol(), 2, 2);
+
+const winner = gameController.determineWinner();
+console.log(winner);
