8d5203f68cb3137f023d7a26d811cb6fca8ade07
Michael Chan <michaelalexanderchan@gmail.com>
2023-01-17 21:15:48 -0500

    Implemented full 'determineWinner' function to find a winner of the game

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Module pattern and IIFE practice - Gameboard
 const gameBoard = (() => {
   // Simulate a board using a 2D array
-  const board = [
-    [0, 'x', 'o'],
-    ['x', 'o', 'o'],
-    [0, 'x', 'o'],
+  /* I realized that I was supposed to make board a private variable and provide access
+  functions only after I finished the assignment. */
+  let board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
   ];
 
   // Determines if a place on the board is already occupied or not
@@ -14,12 +16,22 @@ const gameBoard = (() => {
     }
     if (board[row][column] === 0) {
       board[row][column] = symbol;
+    } else {
+      console.log('spot taken lol');
     }
   };
 
+  const resetBoard = () => {
+    board = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+  };
   return {
     board,
     addMove,
+    resetBoard,
   };
 })();
 
@@ -44,59 +56,75 @@ const gameController = ((board) => {
   };
 
   const determineWinner = () => {
+    // Note: this function is horribly unoptimized :(
     const currentBoard = board.board;
     let winStatus;
-    console.log(`Array length = ${currentBoard.length}`);
-    console.log(`Row length = ${currentBoard[0].length}`);
-    console.log(`Value = ${currentBoard[0][0]}`);
 
-    // // Loop through each row
-    // for (let i = 0; i < currentBoard.length; i += 1) {
-    //   // Loop through each value in a row
-    //   // eslint-disable-next-line prefer-destructuring
-    //   winStatus = currentBoard[i][0];
-    //   for (let j = 1; j < currentBoard[i].length; j += 1) {
-    //     /* If the previous value is not equal to the current value,
-    //     there is no winner for this row and break */
-    //     if (winStatus !== currentBoard[i][j]) {
-    //       winStatus = 'no winner';
-    //       break;
-    //     } else {
-    //       winStatus = currentBoard[i][j];
-    //     }
-    //   }
-    //   if (winStatus !== 'no winner') {
-    //     break;
-    //   }
-    // }
-
-    /*  return winStatus; */
-
-    // // if (winStatus === 'no winner') {
-    // for (let i = 0; i < currentBoard.length; i += 1) {
-    //   // eslint-disable-next-line prefer-destructuring
-    //   winStatus = currentBoard[0][i];
-    //   for (let j = 1; j < currentBoard[i].length; j += 1) {
-    //     if (winStatus !== currentBoard[j][i]) {
-    //       winStatus = 'no winner';
-    //       break;
-    //     } else {
-    //       winStatus = currentBoard[j][i];
-    //     }
-    //   }
-    //   if (winStatus !== 'no winner') {
-    //     break;
-    //   }
-    // }
-    // // }
-
+    // Loop through each row
     for (let i = 0; i < currentBoard.length; i += 1) {
-      winStatus = currentBoard[i][i];
-      for (let j = 1; j < currentBoard.length; j += 1) {
-
+      // Loop through each value in a row
+      // eslint-disable-next-line prefer-destructuring
+      winStatus = currentBoard[i][0];
+      for (let j = 1; j < currentBoard[i].length; j += 1) {
+        /* If the previous value is not equal to the current value,
+        there is no winner for this row and break */
+        if (winStatus !== currentBoard[i][j]) {
+          winStatus = 'no winner';
+          break;
+        } else {
+          winStatus = currentBoard[i][j];
+        }
+      }
+      if (winStatus !== 'no winner') {
+        break;
       }
     }
 
+    if (winStatus === 'no winner') {
+      // Loop through each column
+      for (let i = 0; i < currentBoard.length; i += 1) {
+      // eslint-disable-next-line prefer-destructuring
+        winStatus = currentBoard[0][i];
+        for (let j = 1; j < currentBoard[i].length; j += 1) {
+          if (winStatus !== currentBoard[j][i]) {
+            winStatus = 'no winner';
+            break;
+          } else {
+            winStatus = currentBoard[j][i];
+          }
+        }
+        if (winStatus === 'no winner') {
+          break;
+        }
+      }
+
+      if (winStatus === 'no winner') {
+        // Loop through the diagonal (top-left to bottom-right)
+        for (let i = 0; i < currentBoard.length; i += 1) {
+          winStatus = currentBoard[i][i];
+          for (let j = 1; j < currentBoard.length; j += 1) {
+            if (winStatus !== currentBoard[j][j]) {
+              winStatus = 'no winner';
+              break;
+            } else {
+              winStatus = currentBoard[j][j];
+            }
+          }
+          if (winStatus === 'no winner') {
+            break;
+          }
+        }
+      }
+      // Compare the bottom-left to top-right
+      if (winStatus === 'no winner') {
+        if (currentBoard[0][2] !== currentBoard[2][0]) {
+          winStatus = 'no winner';
+        } else {
+        // eslint-disable-next-line prefer-destructuring
+          winStatus = currentBoard[0][2];
+        }
+      }
+    }
     return winStatus;
   };
 
@@ -117,11 +145,11 @@ const playerFactory = (name, symbol) => {
   };
 };
 
-const player1 = playerFactory('Mikey', 'x');
-const player2 = playerFactory('Richie', 'o');
+const player1 = playerFactory('player1', 'x');
+const player2 = playerFactory('player2', 'o');
 gameController.playerMove(player1.getSymbol(), 2, 1);
-gameController.playerMove(player1.getSymbol(), 2, 1);
+gameController.playerMove(player2.getSymbol(), 2, 1);
 gameController.playerMove(player1.getSymbol(), 2, 2);
 
 const winner = gameController.determineWinner();
-console.log(winner);
+console.log(gameBoard.board);
